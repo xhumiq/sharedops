111e344c150f24a646cc3b4fc04567e521cda127
mchu <mike@omegabc.net>
2020-12-12 05:27:34 -0800

    fix

--- ops/ub_bootstrap.sh
+++ ops/ub_bootstrap.sh
@@ -3,14 +3,16 @@
 
 WS_SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-sudo apt-get dist-upgrade -y
-sudo apt-get update -y
-sudo apt -y install python3-pip
-sudo pip3 install boto3
+apt-get dist-upgrade -y
+apt-get update -y
+apt -y install python3-pip
+pip3 install boto3
 
-nopasswd(){
-  echo "%sudo ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
-}
-mkdir -p ~/.ssh
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCuHZzytEsWbwf/EI7TzCEpRWS2n1eN7RVGzlZYYr+l9+H9c1mZ7u8ID3qbLQ75QFpmmlBwIQ2QCo2ayDoE1O51k+kyjGSMXBg37+hYF8LQW/jLdMbtzffJI/Q68J7E/SfCA7umMAxjzDuUuj2yA5cbS6czkUgtER0yFG1E6meSzyiWbsmP6zbA6pE3YgviTSB2J+mUrVxHjTnMGQy/U9rWIw1K9UTjn934p19Hg4nHTnO022KlSJ73LGMOFcEZXJiBU5wJm3eEEreenVKgFoDJH4opl3+XbbSeDzrB76M0KKi/Ebt0+rPShJ6B3wFpDHD3qkiN1BSBlpitAHuxcbo9 mchu@OBC-IT" > ~/.ssh/authorized_keys
-sudo nopasswd
+mkdir -p /home/mchu/.ssh
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCuHZzytEsWbwf/EI7TzCEpRWS2n1eN7RVGzlZYYr+l9+H9c1mZ7u8ID3qbLQ75QFpmmlBwIQ2QCo2ayDoE1O51k+kyjGSMXBg37+hYF8LQW/jLdMbtzffJI/Q68J7E/SfCA7umMAxjzDuUuj2yA5cbS6czkUgtER0yFG1E6meSzyiWbsmP6zbA6pE3YgviTSB2J+mUrVxHjTnMGQy/U9rWIw1K9UTjn934p19Hg4nHTnO022KlSJ73LGMOFcEZXJiBU5wJm3eEEreenVKgFoDJH4opl3+XbbSeDzrB76M0KKi/Ebt0+rPShJ6B3wFpDHD3qkiN1BSBlpitAHuxcbo9 mchu@OBC-IT" > /home/mchu/.ssh/authorized_keys
+chown -R mchu:mchu /home/mchu/.ssh
+chmod 700 /home/mchu/.ssh
+chmod 600 /home/mchu/.ssh/authorized_keys
+
+echo "%sudo ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
+service sshd restart
