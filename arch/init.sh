987bd79391332e0874b305ab60698b150ccd058e
Mike Chu <mike@omegabc.net>
2022-11-18 11:19:29 -0800

    fix

--- arch/init.sh
+++ arch/init.sh
@@ -6,8 +6,8 @@ sudo pacman -Syuuu
 #--noconfirm -- Kedenlive
 sudo pacman -S --needed i3-gaps i3lock i3status gvfs-smb alsa-utils alsa-card-profiles 
 sudo pacman -S --needed pipewire-pulse pulsemixer pipewire-x11-bell exfat-utils
-sudo pacman -S --needed pcmanfm udiskie base-devel zip unzip polkit
-sudo pacman -S --needed openssh picom polybar chromium go git firefox exfat-utils
+sudo pacman -S --needed pcmanfm udiskie base-devel zip unzip polkit vim
+sudo pacman -S --needed openssh picom polybar chromium go firefox exfat-utils
 sudo pacman -S --needed adobe-source-han-sans-cn-fonts adobe-source-han-sans-tw-fonts
 sudo pacman -S --needed bpytop ttf-firacode-nerd ttf-iosevka-nerd rtkit
 sudo pacman -S --needed dnsutils tcpdump feh rustup cargo ripgrep lxsession
@@ -24,28 +24,44 @@ sudo pacman -S --needed nmap p7zip ranger rsync codeblocks dust exa tmux tmuxp
 sudo pacman -S --needed perl-async-interrupt perl-ev perl-guard perl-json perl-json-xs perl-net-ssleay
 sudo pacman -S --needed bridge-utils virt-manager qemu vde2 ebtables dnsmasq
 
+sudo usermod -aG wheel,audio,video,storage $USER
 
 #sudo pacman -S nvidia-utils nvidia-settings xorg-server xorg-apps xorg-xinit i3gaps numlockx -noconfirm -needed
 
 #sudo pacman -S lightdm lightdm-gtk-greeter --noconfirm --needed
 
-(
+[[ ! -f /usr/bin/yay ]] && (
     cd /tmp
     git clone https://aur.archlinux.org/yay.git
     cd yay
     makepkg -si
 )
 
+yay -Syuuu
+
+ysy() {
+  yay -S --overwrite --redownloadall --removemake --rebuildall --noeditmenu --nodiffmenu --cleanafter --answerclean a --answerupgrade a $@
+}
+
 ysy --needed aur/teamviewer aur/nordvpn-bin aur/visual-studio-code-bin community/capitaine-cursors aur/mint-themes aur/plasma5-themes-breath
 
 if [ ! -d "$HOME/.nvm" ]; then
 	  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
-	  nvm install 16.17.0
 fi
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+
+[ -s "$NVM_DIR/nvm.sh" ] && nvm install 18.12.1
+
 if [ ! -f "/home/mchu/.gvm" ]; then
-#	  zsh < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+	  zsh < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+    [[ -s "${HOME}/.gvm/scripts/gvm" ]] && source "${HOME}/.gvm/scripts/gvm"
 fi
+
+[[ -s "${HOME}/.gvm/scripts/gvm" ]] && gvm install go1.19
 
 if [ ! -f "/home/mchu/.jabba/jabba.sh" ]; then
   curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh
@@ -55,6 +71,3 @@ if [ ! -f "/home/mchu/.jabba/jabba.sh" ]; then
 fi
 
 pip3 install importlib_resources j2cli s4cmd jq yq linode-cli
-
-gvm install go1.19
-nvm install 18.12.1
