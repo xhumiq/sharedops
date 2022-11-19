9edc5baad84c8007ce938be063e7670a714642fa
Mike Chu <mike@omegabc.net>
2022-11-19 07:31:19 -0800

    fix

---
+++ arch/init_76.sh
@@ -0,0 +1,15 @@
+sudo pacman -Syuuu
+
+sudo pacman -S --needed --noconfirm community/pop-icon-theme community/pop-gtk-theme
+
+ysy() {
+  yay -S --overwrite --redownloadall --removemake --rebuildall --noeditmenu --nodiffmenu --cleanafter --answerclean a --answerupgrade a $@
+}
+
+yay -Syuuu
+
+ysy --needed aur/system76-driver aur/system76-keyboard-configurator aur/system76-power 
+ysy --needed aur/system76-firmware-daemon aur/system76-firmware aur/pop-sound-theme-bin
+ysy --needed aur/gnome-shell-extension-system76-power-git aur/system76-dkms 
+ysy --needed aur/system76-scheduler aur/system76-acpi-dkms aur/system76-io-dkms 
+ysy --needed aur/system76-scheduler-git aur/system76-kbd-led-git
