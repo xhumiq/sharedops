012a75570f3ef902b2ea1650c8156e6a731f76d5
Mike Chu <mike@omegabc.net>
2022-11-19 07:25:08 -0800

    fix

--- arch/init.sh
+++ arch/init.sh
@@ -3,31 +3,38 @@ set -e
 
 sudo pacman -Syuuu
 
-sudo pacman -S --needed --noconfirm pciutils lshw pacman-contrib rsync reflector ranger polkit udiskie udisks2 gvfs-smb vim exfat-utils
+sudo pacman -S --needed --noconfirm git pciutils lshw pacman-contrib rsync reflector ranger polkit udiskie udisks2 gvfs-smb vim exfat-utils ntfs-3g
 
-[[ ! pgrep -x reflector ]] && reflector --latest 200 --number 5 --sort rate --save /etc/pacman.d/mirrorlist  > /dev/null &
+[[ ! `pgrep -x reflector` ]] && reflector --latest 200 --number 5 --sort rate --save /etc/pacman.d/mirrorlist  > /dev/null &
 #--noconfirm -- Kedenlive
-sudo pacman -S --needed --noconfirm i3-gaps i3lock i3status alsa-utils alsa-card-profiles 
-sudo pacman -S --needed --noconfirm pipewire-pulse pulsemixer pipewire-x11-bell python
+sudo pacman -S --needed --noconfirm i3-gaps i3lock i3status alsa-utils alsa-card-profiles zsh
+sudo pacman -S --needed --noconfirm pipewire-pulse pulsemixer pipewire-x11-bell python gettext rustup
 sudo pacman -S --needed --noconfirm pcmanfm udiskie base-devel zip unzip bpytop rtkit element-desktop
-sudo pacman -S --needed --noconfirm openssh picom polybar chromium go firefox alacritty aur/joplin-appimage
-sudo pacman -S --needed --noconfirm dnsutils tcpdump feh rustup cargo ripgrep lxsession aur/raysession-git
+sudo pacman -S --needed --noconfirm openssh picom polybar chromium go firefox alacritty openvpn
+sudo pacman -S --needed --noconfirm dnsutils tcpdump feh rustup cargo ripgrep lxsession cuda
 sudo pacman -S --needed --noconfirm bat exa fd procs hexyl sd iftop nload nmon bmon iptraf-ng 
-sudo pacman -S --needed --noconfirm opendesktop-fonts yq broot sd traceroute pavucontrol
+sudo pacman -S --needed --noconfirm opendesktop-fonts yq broot traceroute pavucontrol flameshot
 sudo pacman -S --needed --noconfirm vlc zstd python-pip pygmentize ttf-hack-nerd xorg-xrandr
 sudo pacman -S --needed --noconfirm jq bash-completion keychain mlocate wget curl words lsof which
-sudo pacman -S --needed --noconfirm rxvt-unicode rofi urxvt-perls arandr keepassxc docker-machine
+sudo pacman -S --needed --noconfirm rxvt-unicode rofi urxvt-perls arandr keepassxc docker docker-compose
 sudo pacman -S --needed --noconfirm conky atool highlight elinks mediainfo w3m ffmpegthumbnailer mupdf
-sudo pacman -S --needed --noconfirm dmenu perl-json-xs perl-anyevent-i3 gtk-engines docker docker-compose
+sudo pacman -S --needed --noconfirm dmenu perl-json-xs perl-anyevent-i3 gtk-engines docker-machine
 sudo pacman -S --needed --noconfirm nmap p7zip dust exa tmux tmuxp qemu-full remmina freerdp
 sudo pacman -S --needed --noconfirm perl-async-interrupt perl-ev perl-guard perl-json perl-json-xs perl-net-ssleay
 sudo pacman -S --needed --noconfirm bridge-utils virt-manager vde2 ebtables dnsmasq thunderbird
+sudo pacman -S --needed --noconfirm yubico-pam yubikey-personalization sysstat ps_mem gtop nmon bpytop
+sudo pacman -S --needed --noconfirm dnsutils tcpdump wireshark-qt python-pdftotext xbindkeys strace
 sudo pacman -S --needed --noconfirm noto-fonts ttf-ubuntu-font-family lxappearance signal-desktop
-sudo pacman -S --needed --noconfirm ttf-droid ttf-terminus-nerd ttf-font-awesome ttf-dejavu ttf-freefont
 sudo pacman -S --needed --noconfirm adobe-source-han-sans-cn-fonts adobe-source-han-sans-tw-fonts
-sudo pacman -S --needed --noconfirm ttf-firacode-nerd ttf-iosevka-nerd
+sudo pacman -S --needed --noconfirm ttf-firacode-nerd ttf-iosevka-nerd ttf-dejavu ttf-freefont
 
-sudo usermod -aG wheel,audio,video,storage $USER
+sudo usermod -aG wheel,audio,video,storage,docker $USER
+
+sudo systemctl enable libvirtd.service
+#sudo systemctl start libvirtd.service
+
+sudo systemctl enable docker.service
+#sudo systemctl start docker.service
 
 #sudo pacman -S nvidia-utils nvidia-settings xorg-server xorg-apps xorg-xinit i3gaps numlockx -noconfirm -needed
 
@@ -46,8 +53,19 @@ ysy() {
   yay -S --overwrite --redownloadall --removemake --rebuildall --noeditmenu --nodiffmenu --cleanafter --answerclean a --answerupgrade a $@
 }
 
-ysy --needed --noconfirm aur/teamviewer aur/nordvpn-bin aur/visual-studio-code-bin community/capitaine-cursors aur/mint-themes aur/plasma5-themes-breath
-ysy --needed --noconfirm aur/brave-bin aur/skypeforlinux-stable-bin aur/pavumeter
+[ ! -f /usr/bin/code ] && ysy --needed aur/visual-studio-code-bin
+[ ! -f /usr/bin/nordvpn ] && ysy --needed aur/nordvpn-bin
+[ ! -f /usr/bin/teamviewer ] && ysy --needed aur/teamviewer
+[ ! -f /usr/bin/brave ] && ysy --needed aur/brave-bin
+[ ! -f /usr/bin/skypeforlinux ] && ysy --needed aur/skypeforlinux-stable-bin
+[ ! -f /usr/bin/pavumeter ] && ysy --needed aur/pavumeter
+[ ! -f /usr/bin/joplin-desktop ] && ysy --needed aur/joplin-appimage 
+[ ! -f /usr/bin/joplin-desktop ] && ysy --needed aur/raysession-git
+[ ! -f /usr/bin/mpv ] && ysy --needed aur/mpv-full
+[ ! -f /usr/bin/ffmpeg ] && ysy --needed aur/ffmpeg-full
+[ ! -d /usr/share/icons/capitaine-cursors ] && ysy --needed community/capitaine-cursors
+[ ! -d /usr/share/themes/Mint-X ] && ysy --needed aur/mint-themes
+[ ! -d /usr/share/icons/breeze ] && ysy --needed --noconfirm aur/plasma5-themes-breath
 
 if [ ! -d "$HOME/.nvm" ]; then
 	  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
